805598fc747e17e5eefac809a0468a036b930059
Sergey Popov <sergobot256@gmail.com>
2015-06-26 14:55:42 +0300

    Add class RWindow

---
+++ src/RWindow.cpp
@@ -0,0 +1,95 @@
+/**
+ * This file is part of Realio.
+ * Realio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2015 Sergey Popov <sergobot@vivaldi.net>
+**/
+
+//Realio
+#include "RWindow.h"
+
+namespace Realio {
+RWindow::RWindow(std::string title)
+{
+    m_title = new std::string;
+    *m_title = title;
+
+    if(!initializeSdl())
+    {
+        std::cerr << "Exiting.\n";
+        std::exit(1);
+    }
+}
+
+RWindow::~RWindow()
+{
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+    delete m_title;
+}
+
+bool RWindow::initializeSdl()
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
+    {
+        std::cerr << "Could not initialize SDL: " << SDL_GetError();
+        std::cerr << std::endl;
+        std::exit(1);
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+
+    SDL_DisplayMode current;
+
+    if(SDL_GetCurrentDisplayMode(0, &current) != 0)
+    {
+        // In case of error...
+        std::cerr << "Could not get display mode for video display: ";
+        std::cerr << SDL_GetError() << std::endl;
+        return false;
+    }
+    else
+    {
+        // Everything is normal
+        m_width = current.w;
+        m_height = current.h;
+    }
+
+    m_window = SDL_CreateWindow(
+                m_title->data(),
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                m_width, m_height,
+                SDL_WINDOW_OPENGL |
+                SDL_WINDOW_FULLSCREEN_DESKTOP |
+                SDL_WINDOW_HIDDEN);
+
+    return true;
+}
+
+void RWindow::show()
+{
+    SDL_ShowWindow(m_window);
+}
+
+void RWindow::close()
+{
+    SDL_DestroyWindow(m_window);
+}
+
+void RWindow::hide()
+{
+    SDL_HideWindow(m_window);
+}
+}
---
+++ src/RWindow.h
@@ -0,0 +1,65 @@
+/**
+ * This file is part of Realio.
+ * Realio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2015 Sergey Popov <sergobot@vivaldi.net>
+**/
+
+//C++
+#include <iostream>
+#include <string>
+//SDL2
+#include <SDL2/SDL.h>
+
+namespace Realio {
+class RWindow
+{
+public:
+    RWindow(const std::string title);
+    ~RWindow();
+
+    /**
+     * @brief shows the window.
+     * @param void.
+     * @return void.
+     */
+    void show();
+
+    /**
+     * @brief closes the window.
+     * @param void.
+     * @return void.
+     */
+    void close();
+
+    /**
+     * @brief hides the window.
+     * @param void.
+     * @return void.
+     */
+    void hide();
+
+private:
+    std::string *m_title;
+    SDL_Window* m_window;
+
+    int m_width;
+    int m_height;
+
+    /**
+     * @brief initializes SDL.
+     * @param void.
+     * @return True, if SDL is initialized. False, if not.
+     */
+    bool initializeSdl();
+};
+}
