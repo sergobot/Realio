03e9dc2d5907a60b72a5f2956f92a92609c5a123
Sergey Popov <sergobot@vivaldi.net>
2015-11-01 21:57:09 +0300

    Add ability to generate shaders in realtime

--- src/RShader.cpp
+++ src/RShader.cpp
@@ -22,12 +22,54 @@
 namespace Realio {
 RShader::RShader()
 {
-
+    initVariableTypes();
 }
 
-RShader::RShader(const char *vShader, const char *fShader)
+RShader::RShader(const char *vShader, const char *fShader, const char *gShader)
 {
-    compileShaders(vShader, fShader);
+    compileShaders(vShader, fShader, gShader);
+}
+
+void RShader::initVariableTypes()
+{
+    m_variableTypes = {
+        {RSHADER_BOOL_VARIABLE,         "bool"},
+        {RSHADER_INT_VARIABLE,          "int"},
+        {RSHADER_UINT_VARIABLE,         "uint"},
+        {RSHADER_FLOAT_VARIABLE,        "float"},
+
+        {RSHADER_BVEC2_VARIABLE,        "bvec2"},
+        {RSHADER_BVEC3_VARIABLE,        "bvec3"},
+        {RSHADER_BVEC4_VARIABLE,        "bvec4"},
+
+        {RSHADER_IVEC2_VARIABLE,        "ivec2"},
+        {RSHADER_IVEC3_VARIABLE,        "ivec3"},
+        {RSHADER_IVEC4_VARIABLE,        "ivec4"},
+
+        {RSHADER_UVEC2_VARIABLE,        "uvec2"},
+        {RSHADER_UVEC3_VARIABLE,        "uvec3"},
+        {RSHADER_UVEC4_VARIABLE,        "uvec4"},
+
+        {RSHADER_VEC2_VARIABLE,         "vec2"},
+        {RSHADER_VEC3_VARIABLE,         "vec3"},
+        {RSHADER_VEC4_VARIABLE,         "vec4"},
+
+        {RSHADER_MAT2X2_VARIABLE,       "mat2x2"},
+        {RSHADER_MAT2X3_VARIABLE,       "mat2x3"},
+        {RSHADER_MAT2X4_VARIABLE,       "mat2x4"},
+
+        {RSHADER_MAT3X2_VARIABLE,       "mat3x2"},
+        {RSHADER_MAT3X3_VARIABLE,       "mat3x3"},
+        {RSHADER_MAT3X4_VARIABLE,       "mat3x4"},
+
+        {RSHADER_MAT4X2_VARIABLE,       "mat4x2"},
+        {RSHADER_MAT4X3_VARIABLE,       "mat4x3"},
+        {RSHADER_MAT4X4_VARIABLE,       "mat4x4"},
+
+        {RSHADER_SAMPLER1D_VARIABLE,    "Sampler1D"},
+        {RSHADER_SAMPLER2D_VARIABLE,    "Sampler2D"},
+        {RSHADER_SAMPLER3D_VARIABLE,    "Sampler3D"}
+    };
 }
 
 void RShader::use()
@@ -35,10 +77,10 @@ void RShader::use()
     glUseProgram(m_program);
 }
 
-void RShader::compileShaders(const char *vShader, const char *fShader)
+void RShader::compileShaders(const char *vShader, const char *fShader, const char *gShader)
 {
     // Compile shaders
-    GLuint vertex, fragment;
+    GLuint vertex, fragment, geometry;
     GLint success;
     GLchar infoLog[512];
 
@@ -70,6 +112,24 @@ void RShader::compileShaders(const char *vShader, const char *fShader)
     m_program = glCreateProgram();
     glAttachShader(m_program, vertex);
     glAttachShader(m_program, fragment);
+
+    //Geometry Shader
+    if(gShader != nullptr)
+    {
+        geometry = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry, 1, &gShader, NULL);
+        glCompileShader(geometry);
+        // Print compile errors if any
+        glGetShaderiv(geometry, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(geometry, 512, NULL, infoLog);
+            std::cerr << "Could not compile geometry shader: " << infoLog << std::endl;
+        }
+
+        glAttachShader(m_program, geometry);
+    }
+
     glLinkProgram(m_program);
     // Print linking errors if any
     glGetProgramiv(m_program, GL_LINK_STATUS, &success);
@@ -82,6 +142,127 @@ void RShader::compileShaders(const char *vShader, const char *fShader)
     // Delete the shaders as they're linked into our program now and no longer necessary
     glDeleteShader(vertex);
     glDeleteShader(fragment);
+    if(gShader != nullptr)
+        glDeleteShader(geometry);
+}
+
+void RShader::compileShaders()
+{
+    //Concatenate data to the vertex shader
+    m_vertexShader.shader = "#version 330 core\n";
+    m_vertexShader.shader.append(m_vertexShader.inputVariables);
+    m_vertexShader.shader.append(m_vertexShader.outputVariables);
+    m_vertexShader.shader.append(m_vertexShader.uniforms);
+    m_vertexShader.shader.append("void main() {\n");
+    m_vertexShader.shader.append(m_vertexShader.mainFunction + "}");
+
+    //Concatenate data to the fragment shader
+    m_fragmentShader.shader = "#version 330 core\n";
+    m_fragmentShader.shader.append(m_fragmentShader.inputVariables);
+    m_fragmentShader.shader.append(m_fragmentShader.outputVariables);
+    m_fragmentShader.shader.append(m_fragmentShader.uniforms);
+    m_fragmentShader.shader.append("void main() {\n");
+    m_fragmentShader.shader.append(m_fragmentShader.mainFunction + "}");
+
+    //Concatenate data to the geometry shader
+    m_geometryShader.shader = "#version 330 core\n";
+    m_geometryShader.shader.append(m_geometryShader.inputVariables);
+    m_geometryShader.shader.append(m_geometryShader.outputVariables);
+    m_geometryShader.shader.append(m_geometryShader.uniforms);
+    m_geometryShader.shader.append("void main() {\n");
+    m_geometryShader.shader.append(m_geometryShader.mainFunction + "}");
+
+    compileShaders(m_vertexShader.shader.c_str(), m_fragmentShader.shader.c_str(), m_geometryShader.shader.c_str());
+}
+
+void RShader::addUniform(const char *name, RShaderVariableType variableType, RShaderShaderType shaderType)
+{
+    if(shaderType == RSHADER_VERTEX_SHADER)
+        m_vertexShader.uniforms.append("uniform " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_GEOMETRY_SHADER)
+        m_geometryShader.uniforms.append("uniform " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_FRAGMENT_SHADER)
+        m_fragmentShader.uniforms.append("uniform " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else
+        std::cerr << "Could not add uniform variable '" << name << "' to any shader: Wrong shader type.\n";
+}
+
+int RShader::addInputVariable(const char *name, RShaderVariableType variableType, RShaderShaderType shaderType, bool located)
+{
+    if(shaderType == RSHADER_VERTEX_SHADER)
+        if(located) 
+        {
+            std::string variable(name);
+            std::size_t i = variable.find('[');
+            unsigned loc = m_vertexShader.location;
+            if(i != std::string::npos)
+            {
+                m_vertexShader.inputVariables.append("layout (location = " + std::to_string(loc) + ") in " +
+                                                 m_variableTypes.at(variableType) +
+                                                 " " + name + ";\n");
+                m_vertexShader.location += std::stoi(std::string(variable.begin() + i + 1, variable.begin() + variable.find(']') - 1));
+            }
+            else
+                m_vertexShader.location += 1;
+            
+            return loc;
+        }
+        else
+            m_vertexShader.inputVariables.append("in " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_GEOMETRY_SHADER)
+        m_geometryShader.inputVariables.append("in " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_FRAGMENT_SHADER)
+        m_fragmentShader.inputVariables.append("in " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else
+        std::cerr << "Could not add input variable '" << name << "' to any shader: Wrong shader type.\n";
+    
+    return -1;
+}
+
+int RShader::addOutputVariable(const char *name, RShaderVariableType variableType, RShaderShaderType shaderType, bool located)
+{
+    if(shaderType == RSHADER_VERTEX_SHADER)
+        m_vertexShader.outputVariables.append("out " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_GEOMETRY_SHADER)
+        m_geometryShader.outputVariables.append("out " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    else if(shaderType == RSHADER_FRAGMENT_SHADER)
+    {
+        if(located) 
+        {
+            std::string variable(name);
+            std::size_t i = variable.find('[');
+            unsigned loc = m_fragmentShader.location;
+            if(i != std::string::npos)
+            {
+                m_fragmentShader.outputVariables.append("layout (location = " + std::to_string(loc) + ") out " +
+                                                 m_variableTypes.at(variableType) +
+                                                 " " + name + ";\n");
+                m_fragmentShader.location += std::stoi(std::string(variable.begin() + i + 1, variable.begin() + variable.find(']') - 1));
+            }
+            else
+                m_fragmentShader.location += 1;
+            
+            return loc;
+        }
+        else
+            m_fragmentShader.outputVariables.append("out " + m_variableTypes.at(variableType) + " " + name + ";\n");
+    }
+    else
+        std::cerr << "Could not add output variable '" << name << "' to any shader: Wrong shader type.\n";
+    
+    return -1;
+}
+
+void RShader::addAction(const char *line, RShaderShaderType shaderType)
+{
+    if(shaderType == RSHADER_VERTEX_SHADER)
+        m_vertexShader.mainFunction.append(line);
+    else if(shaderType == RSHADER_GEOMETRY_SHADER)
+        m_geometryShader.mainFunction.append(line);
+    else if(shaderType == RSHADER_FRAGMENT_SHADER)
+        m_fragmentShader.mainFunction.append(line);
+    else
+        std::cerr << "Could not add line '"<< line << "' to any shader: Wrong shader type.\n";
 }
 
 GLuint RShader::getProgram()
--- src/RShader.h
+++ src/RShader.h
@@ -17,18 +17,68 @@
 #ifndef RSHADER_H
 #define RSHADER_H
 
+//C++
+#include <string>
+#include <vector>
+#include <map>
 //GLEW
 #include <GL/glew.h>
 
 namespace Realio {
+enum RShaderShaderType {
+    RSHADER_VERTEX_SHADER,   //Vertex shader
+    RSHADER_GEOMETRY_SHADER, //Geometry shader
+    RSHADER_FRAGMENT_SHADER  //Fragment shader
+};
+
+enum RShaderVariableType {
+    //Basic scalar types
+    RSHADER_BOOL_VARIABLE,
+    RSHADER_INT_VARIABLE,
+    RSHADER_UINT_VARIABLE,
+    RSHADER_FLOAT_VARIABLE,
+    //Boolean vector
+    RSHADER_BVEC2_VARIABLE,
+    RSHADER_BVEC3_VARIABLE,
+    RSHADER_BVEC4_VARIABLE,
+    //Integer vector
+    RSHADER_IVEC2_VARIABLE,
+    RSHADER_IVEC3_VARIABLE,
+    RSHADER_IVEC4_VARIABLE,
+    //Unsigned integer vector
+    RSHADER_UVEC2_VARIABLE,
+    RSHADER_UVEC3_VARIABLE,
+    RSHADER_UVEC4_VARIABLE,
+    //Floating point vector
+    RSHADER_VEC2_VARIABLE,
+    RSHADER_VEC3_VARIABLE,
+    RSHADER_VEC4_VARIABLE,
+    //Floating point matrix with 2 columns
+    RSHADER_MAT2X2_VARIABLE,
+    RSHADER_MAT2X3_VARIABLE,
+    RSHADER_MAT2X4_VARIABLE,
+    //Floating point matrix with 3 columns
+    RSHADER_MAT3X2_VARIABLE,
+    RSHADER_MAT3X3_VARIABLE,
+    RSHADER_MAT3X4_VARIABLE,
+    //Floating point matrix with 4 columns
+    RSHADER_MAT4X2_VARIABLE,
+    RSHADER_MAT4X3_VARIABLE,
+    RSHADER_MAT4X4_VARIABLE,
+    //Sapmlers
+    RSHADER_SAMPLER1D_VARIABLE,
+    RSHADER_SAMPLER2D_VARIABLE,
+    RSHADER_SAMPLER3D_VARIABLE,
+};
+
 class RShader
 {
 public:
-    RShader(const char *vShader, const char *fShader);
+    RShader(const char *vShader, const char *fShader, const char *gShader = nullptr);
     RShader();
 
     /**
-     * @brief uses the shader.
+     * @brief uses shader program.
      * @param void.
      * @return void.
      */
@@ -39,7 +89,42 @@ public:
      * @param vertex and fragment shaders' sources.
      * @return void.
      */
-    void compileShaders(const char *vShader, const char *fShader);
+    void compileShaders(const char *vShader, const char *fShader, const char *gShader = nullptr);
+
+    /**
+     * @brief concatenates data in m_***Shaders to shaders, compiles and links them to a shader program using function above.
+     * @param void.
+     * @return void.
+     */
+    void compileShaders();
+
+    /**
+     * @brief adds a uniform variable to the shader.
+     * @param name of the variable, its type and shader's type, to which the variable is added.
+     * @return void.
+     */
+    void addUniform(const char* name, RShaderVariableType variableType, RShaderShaderType shaderType);
+
+    /**
+     * @brief adds an input variable to the shader.
+     * @param name of the variable, its type and shader's type, to which the variable is added and location trigger.
+     * @return if shader is vertex and located = true returns location, else returns -1.
+     */
+    int addInputVariable(const char* name, RShaderVariableType variableType, RShaderShaderType shaderType, bool located = true);
+
+    /**
+     * @brief adds an output variable to the shader.
+     * @param name of the variable, its type and shader's type, to which the variable is added and location trigger.
+     * @return if shader is fragment and located = true returns location, else returns -1.
+     */
+    int addOutputVariable(const char* name, RShaderVariableType variableType, RShaderShaderType shaderType, bool located = true);
+    
+    /**
+     * @brief adds a line to the main function of the shader.
+     * @param line to be executed in the main function of the shader and shader's type, to which the line is added.
+     * @return void.
+     */
+    void addAction(const char* line, RShaderShaderType shaderType);
 
     /**
      * @brief returns the program.
@@ -51,12 +136,34 @@ public:
     /**
      * @brief returns the program.
      * @param void.
-     * @return current shader program, placed in GLuint.
+     * @return void.
      */
     void deleteProgram();
 
 private:
+    struct Shader {
+        std::string shader;
+        std::string uniforms;
+        std::string inputVariables;
+        std::string outputVariables;
+        std::string mainFunction;
+        unsigned location = 0;
+    };
+
+    Shader m_vertexShader;
+    Shader m_geometryShader;
+    Shader m_fragmentShader;
+
+    std::map<RShaderVariableType, std::string> m_variableTypes;
+
     GLuint m_program;
+
+    /**
+     * @brief fills m_variableTypes.
+     * @param void.
+     * @return void.
+     */
+    void initVariableTypes();
 };
 }
 
